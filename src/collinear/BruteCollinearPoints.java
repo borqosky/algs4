70730c5e2c4dc909cf74356dc019caf56633be2b
Wlodzimierz Borkowski <wlodek.borkowski@gmail.com>
2018-01-29 10:15:53 +0100

    template for collinear points

---
+++ src/collinear/BruteCollinearPoints.java
@@ -0,0 +1,14 @@
+package collinear;
+
+public class BruteCollinearPoints {
+    public BruteCollinearPoints(Point[] points) {  // finds all line segments containing 4 points
+
+    }
+
+    public int numberOfSegments() {  // the number of line segments
+        return 0;
+    }
+    public LineSegment[] segments() {  // the line segments
+        return new LineSegment[0];
+    }
+}
---
+++ test/collinear/CollinearTest.java
@@ -0,0 +1,10 @@
+package collinear;
+
+import org.junit.jupiter.api.Test;
+
+public class CollinearTest {
+    @Test
+    void test_check_size() {
+
+    }
+}
