1037f1b7404081e8aae64da989738ed792d330ad
Wlodzimierz Borkowski <wlodek.borkowski@gmail.com>
2018-01-31 07:41:51 +0100

    fast collinear implemented

--- src/collinear/FastCollinearPoints.java
+++ src/collinear/FastCollinearPoints.java
@@ -4,16 +4,38 @@ import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.StdOut;
 import edu.princeton.cs.algs4.StdDraw;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class FastCollinearPoints {
+    private  int n;
+    private List<LineSegment> lineSegments;
+
     public FastCollinearPoints(Point[] points) {  // finds all line segments containing 4 or more points
+        if (points == null)
+            throw new IllegalArgumentException();
+
+        n = 0;
+        lineSegments = new ArrayList<>();
+
+        Arrays.sort(points, points[0].slopeOrder());
+
+        int c = 1;
+        for (int p = 0; p < points.length - 3; p++) {
+            while (points[p] == points[c])
+                c++;
+            if (c >= 3)
+                lineSegments.add(new LineSegment(points[p], points[c]));
+        }
     }
 
     public int numberOfSegments() {  // the number of line segments
-        return 0;
+        return n;
     }
 
     public LineSegment[] segments() {  // the line segments
-        return new LineSegment[0];
+        return lineSegments.toArray(new LineSegment[lineSegments.size()]);
     }
 
     public static void main(String[] args) {
